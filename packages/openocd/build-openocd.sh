39360e52c441283f341f2b129caf7988a11bfd2e
Nikhil Dabas <nikhil@nikhildabas.com>
2023-01-27 13:18:08 +0000

    Update OpenOCD build

--- packages/openocd/build-openocd.sh
+++ packages/openocd/build-openocd.sh
@@ -4,14 +4,6 @@ set -euo pipefail
 
 BITNESS=$1
 ARCH=$2
-OPENOCD_BRANCH="master"
-
-if [ ! -d openocd ]; then
-  git clone "https://github.com/openocd-org/openocd.git" -b $OPENOCD_BRANCH --depth=1
-else
-  git -C openocd checkout -B $OPENOCD_BRANCH
-  git -C openocd pull --ff-only
-fi
 
 cd openocd
 ./bootstrap
