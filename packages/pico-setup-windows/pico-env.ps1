5040fee193644392c8c88c83f352891bd3efe716
Nikhil Dabas <nikhil@nikhildabas.com>
2024-08-12 20:26:45 +0100

    Remove COMSPEC
    Fixes https://github.com/raspberrypi/pico-setup-windows/issues/36

--- packages/pico-setup-windows/pico-env.ps1
+++ packages/pico-setup-windows/pico-env.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 
 # https://github.com/microsoft/vswhere/wiki/Start-Developer-Command-Prompt#using-powershell
-& "${env:COMSPEC}" /s /c "`"$PSScriptRoot\pico-env.cmd`" && set" | ForEach-Object {
+cmd.exe /s /c "`"$PSScriptRoot\pico-env.cmd`" && set" | ForEach-Object {
   $name, $value = $_ -split '=', 2
   Set-Content env:\"$name" $value
 }
